1f8788c5d028cf1424e8e4224728d3f29a7b6071
LeonardoHTPE12 <leonardoalvesalves09@gmail.com>
2019-08-22 22:15:25 -0300

    Criação das tabelas em adamento

--- src/main/webapp/database.sql
+++ src/main/webapp/database.sql
@@ -1,8 +1,23 @@
+
 CREATE TABLE pessoas (
 	id	BIGINT NOT	NULL AUTO_INCREMENT,
-	nome			VARCHAR(255),
+	Nome			VARCHAR(255),
+	Data nascimento VARCHAR(255),
+	Sexo 			VARCHAR(255),
+	
+	PRIMARY KEY 	(id)
+);
+
+CREATE TABLE endereco (
+	id	BIGINT NOT	NULL AUTO_INCREMENT,
+	Logradouro		VARCHAR(255),
+	CEP				VARCHAR(255),
+	PRIMARY KEY 	(id)
+);
+
+CREATE TABLE contato (
+	id	BIGINT NOT	NULL AUTO_INCREMENT,
 	email			VARCHAR(255),
-	endereco		VARCHAR(255),
 	telefone		VARCHAR(255),
 	PRIMARY KEY 	(id)
 );
